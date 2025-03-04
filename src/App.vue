d316550cc4d91adc8d8ed76a731e56bc4ac2e2e5
1024小神 <15670339118@163.com>
2025-03-04 20:45:05 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,20 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { useConfigStore } from './stores/configStore'
+
+const configStore = useConfigStore()
+
+watch(
+    configStore.$state,
+    (state) => {
+        // 每当它发生变化时，将整个状态持久化到本地存储
+        localStorage.setItem('configStore', JSON.stringify(state))
+        console.log('configStore', state)
+    },
+    { deep: true }
+)
 </script>
 
 <style scoped lang="scss"></style>
